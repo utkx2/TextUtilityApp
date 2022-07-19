b8fd2082bcd82de41d559fc58a7aa7f4aca64cd3
utkx2 <somvanshi2132001@gmail.com>
2022-07-19 23:43:25 +0530

    Updates

---
+++ precache-manifest.44facce1bd80fa3501ddbb286eb4a1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1bd96c2de8b7e63f133",
+    "url": "/TextUtilityApp/static/css/main.610cb050.chunk.css"
+  },
+  {
+    "revision": "a1bd96c2de8b7e63f133",
+    "url": "/TextUtilityApp/static/js/main.a1bd96c2.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtilityApp/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtilityApp/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "c771d3893541108f2b7c",
+    "url": "/TextUtilityApp/static/js/runtime~main.c771d389.js"
+  },
+  {
+    "revision": "7aaca25caffd96f4341aafd053698431",
+    "url": "/TextUtilityApp/index.html"
+  }
+];
